c422118074a20deb518052356a549d2b836e257b
victortanzy123 <victortanzy512@gmail.com>
2024-03-07 14:59:19 +0800

    fix landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,7 @@ import { SWITCH_THEME_DURATION } from "@/utils/constants/switch-theme-duration";
 import { PageWrapper } from "@/components/styles/page-wrapper";
 import OverallMarketStatsView from "@/components/views/protocol/OverallMarketStatsView";
 
-async function getData() {
-  const productsResponse = await fetch(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  const products = await productsResponse.json();
-
-  console.log("Here: ", products);
-
-  return products;
-}
-
 export default async function Home() {
-  const products = await getData();
-  console.log("Products: ", products);
-
   return (
     <>
       <PageWrapper>
@@ -39,31 +25,6 @@ export default async function Home() {
             </div>
 
             <OverallMarketStatsView />
-
-            {/* <div className={styles.grid}>
-              <a href="https://beta.nextjs.org/docs" className={styles.card}>
-                <h2>Documentation &rarr;</h2>
-                <p>Find in-depth information about Next.js 13</p>
-              </a>
-
-              <a
-                href="https://github.com/vercel/next.js/tree/canary/examples"
-                className={styles.card}
-              >
-                <h2>Examples &rarr;</h2>
-                <p>Explore the Next.js 13 playground.</p>
-              </a>
-
-              <a
-                href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.card}
-              >
-                <h2>Deploy &rarr;</h2>
-                <p>Deploy your Next.js site to a public URL with Vercel.</p>
-              </a>
-            </div> */}
           </main>
 
           <footer className={styles.footer}>
